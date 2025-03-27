c4a42703abca8c88c5a41fca6856fa15f52d9610
IamShivakumar <shivakumar2k20@gmail.com>
2025-03-27 20:29:19 +0530

    Front-end Vercel config

--- Backend/main/settings.py
+++ Backend/main/settings.py
@@ -152,7 +152,28 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://travel-requestor-2d9v.vercel.app/",
+    "https://travel-requestor-2d9v.vercel.app",
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 
 # OpenAI Configuration
